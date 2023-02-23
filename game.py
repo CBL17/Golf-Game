70e4317899160f9b5b05c7d2ca1b4d5e4bdf1de9
Charlie Schoettle <cschoettle981@gmail.com>
2023-02-23 16:02:28 -0500

    0.1

--- game.py
+++ game.py
@@ -10,9 +10,12 @@ WHITE = ( 255, 255, 255 )
 GREEN = ( 0, 255, 0 )
 RED   = ( 255, 0, 0 )
 
-#Screen Initialization
+#Game Parameters
+FPS = 144
 SCREENWIDTH  = 1280
 SCREENHEIGHT = 720
+
+#Screen Initialization
 size = (SCREENWIDTH, SCREENHEIGHT)
 DISPLAY = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
@@ -20,24 +23,22 @@ clock = pygame.time.Clock()
 #Background
 hole1 = pygame.image.load("utils\Hole 1.png")
 
-#Game Parameters
-FPS = 144
-
 #Ball Initialization
 player1 = Ball(SCREENWIDTH, SCREENHEIGHT)
 
-#Game
+#Game Loop
 carryOn = True
 while carryOn:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             carryOn = False
-            
-    #Game Logic
-    DISPLAY.blit(hole1, (0,0))
+    #Tab Name
     pygame.display.set_caption(f'{clock.get_fps() :.0f}')
 
+    #Game Logic
+    DISPLAY.blit(hole1, (0,0))
     player1.draw(DISPLAY, WHITE)
+    player1.barrierCheck()
     player1.move()
     player1.swing()
     
